e0c68b7f0d71619ea72813f49cce0135fea8eb05
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2023-11-02 23:06:31 +0530

    Assignment 14,15

---
+++ 2023-11-01/q1.asm
@@ -0,0 +1,18 @@
+LXI H, 2000H;    21 00 20
+MOV B, M;        46
+INX H;           23
+MOV C, M;        4E
+SKIP:
+    INX H;       23
+    MOV A, M;    7E
+    CMP B;       B8
+    JZ LOOP;     CA 16 20
+    DCR C;       0D
+    JNZ SKIP;    C2 06 20
+MVI A, 00H;      3E 00
+STA 2200H;       32 00 22
+HLT;             76
+LOOP:
+    MVI A, 01H;  3E 01
+    STA 2200H;   32 00 22
+    HLT;         76
