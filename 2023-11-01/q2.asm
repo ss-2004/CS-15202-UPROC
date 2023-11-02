0f901e21773f2ce493bf13057a8aa8eda82ff1c5
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2023-11-02 23:30:11 +0530

    Assignment 14,15

--- 2023-11-01/q2.asm
+++ 2023-11-01/q2.asm
@@ -1,58 +1,58 @@
-LDA 3000H;
-MOV B, A;
-MVI A, 0;
-STA 3003H;
-MOV L, A;
-MVI A, 9;
-MOV H, A;
+LDA 3000H;		3A 00 30
+MOV B, A;		47
+MVI A, 0;		3E 00
+STA 3003H;		32 03 30
+MOV L, A;		6F
+MVI A, 9;		3E 09 
+MOV H, A;		67
 
 LOOP_STRT:
-    LDA 3003H;
-    INR A;
-    STA 3003H;
-    MOV A, H;
-    CMP L;
-    JC LOOP_END
-    ADD L;
-    RAR;
-    MOV C, A;
-    JNC RESET
-    CMC;
+    LDA 3003H;	3A 03 30
+    INR A;		3C
+    STA 3003H;	32 03 30
+    MOV A, H;	7C
+    CMP L;		BD
+    JC LOOP_END;DA
+    ADD L;		85
+    RAR;		1F
+    MOV C, A;	4F
+    JNC RESET;	D2 20 00
+    CMC;		3F
 
 RESET:
-    NOP;
-    LXI D, 3010H;
-    ADD E;
-    MOV E, A;
-    MVI A, 0;
-    ADC D;
-    MOV D, A;
-    LDAX D;
-    CMP B;
+    NOP;		
+    LXI D,3010H;11 10 30
+    ADD E;		83
+    MOV E, A;	5F
+    MVI A, 0;	3E 00
+    ADC D;		8A
+    MOV D, A;	57
+    LDAX D;		1A
+    CMP B;		B8
 
-JC ELSE;
-JZ PRINT;
-MOV A, C;
-DCR A;
-MOV H, A;
-JMP LOOP_STRT;
+JC ELSE;		DA
+JZ PRINT;		CA 3D 00
+MOV A, C;		79
+DCR A;			3D
+MOV H, A;		67
+JMP LOOP_STRT;	C3 0D 00
 
-ELSE:
-    MOV A, C;
-    INR A;
-    MOV A, L;
-    JMP LOOP_STRT;
+ELSE:			
+    MOV A, C;	79
+    INR A;		3C
+    MOV A, L;	7D
+    JMP LOOP_STRT;C3 0D 00
 
 PRINT:
-    MVI A, 1;
-    STA 3001H;
-    MOV A, C;
-    STA 3002H;
-    JMP TRUE_END;
+    MVI A, 1;	3E 01
+    STA 3001H;	32 01 30
+    MOV A, C;	79
+    STA 3002H;	32 02 30
+    JMP TRUE_END;C3 4E 00
 
 LOOP_END:
-    MVI A, 2;
-    STA 3001H;
+    MVI A, 2;	3E 02
+    STA 3001H;	32 01 30
 
 TRUE_END:
-    HLT;
+    HLT;		76
