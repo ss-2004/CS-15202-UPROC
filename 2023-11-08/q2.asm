0ae3f70447429124f1d1e30d3c9540b37a743fdb
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2023-12-06 00:51:26 +0530

    Assignment 16, 17

--- 2023-11-08/q2.asm
+++ 2023-11-08/q2.asm
@@ -1,67 +1,69 @@
-4200 3A D0  7	 LDA 2000
-4203 47  	 MOV B, A
-4204 0E 00 	 MVI C, 0
-4206 16 00 	 MVI D, 0
-4208 21 D1  7	 LXI H, 2001
+;Selection Sort
+
+LDA 2000            ;4200 3A D0  7	 
+MOV B, A            ;4203 47  	 
+MVI C, 0            ;4204 0E 00 	 
+MVI D, 0            ;4206 16 00 	 
+LXI H, 2001         ;4208 21 D1  7	 
    	 
-420B 79  	 ILOOP:	MOV A, C
-420C B8  	 	CMP B
-420D CA 63 42	 	JZ END
-4210 79  	 	MOV A, C
-4211 32 DC  7	 	STA 2012
-4214 79  	 	MOV A, C
-4215 3C  	 	INR A
-4216 57  	 	MOV D, A
+ILOOP:	MOV A, C    ;420B 79  	 
+CMP B               ;420C B8  	 	
+JZ END              ;420D CA 63 42	 	
+MOV A, C            ;4210 79  	 	
+STA 2012            ;4211 32 DC  7	 	
+MOV A, C            ;4214 79  	 	
+INR A               ;4215 3C  	 	
+MOV D, A            ;4216 57  	 	
    	 
-4217 7A  	 JLOOP:	MOV A, D
-4218 B8  	 	CMP B
-4219 CA 53 42	 	JZ NXTPSS
-421C 3A DC  7	 	LDA 2012
-421F 21 D1  7	 	LXI H, 2001
-4222 85  	 	ADD L
-4223 6F  	 	MOV L, A
-4224 5E  	 	MOV E, M
-4225 7A  	 	MOV A, D
-4226 21 D1  7	 	LXI H, 2001
-4229 85  	 	ADD L
-422A 6F  	 	MOV L, A
-422B 7E  	 	MOV A, M
-422C BB  	 	CMP E
-422D DA 4C 42	 	JC CHGIDX
+JLOOP:	MOV A, D    ;4217 7A  	 
+CMP B               ;4218 B8  	 	
+JZ NXTPSS           ;4219 CA 53 42	 	
+LDA 2012            ;421C 3A DC  7	 	
+LXI H, 2001         ;421F 21 D1  7	 	
+ADD L               ;4222 85  	 	
+MOV L, A            ;4223 6F  	 	
+MOV E, M            ;4224 5E  	 	
+MOV A, D            ;4225 7A  	 	
+LXI H, 2001         ;4226 21 D1  7	 	
+ADD L               ;4229 85  	 	
+MOV L, A            ;422A 6F  	 	
+MOV A, M            ;422B 7E  	 	
+CMP E               ;422C BB  	 	
+JC CHGIDX           ;422D DA 4C 42	 	
    	 
-4230 21 D1  7	 BACK:	LXI H, 2001
-4233 14  	 	INR D
+BACK:	LXI H, 2001 ;4230 21 D1  7	 
+INR D               ;4233 14  	 	
    	 
-4234 C3 17 42	 BACK2: 	JMP JLOOP
+BACK2: 	JMP JLOOP   ;4234 C3 17 42	 
    	 
-4237 21 D1  7	 SWP:	LXI H, 2001
-423A 79  	 	MOV A, C
-423B 85  	 	ADD L
-423C 6F  	 	MOV L, A
-423D 7E  	 	MOV A, M
-423E 73  	 	MOV M, E
-423F 5F  	 	MOV E, A
-4240 21 D1  7	 	LXI H, 2001
-4243 3A DC  7	 	LDA 2012
-4246 85  	 	ADD L
-4247 6F  	 	MOV L, A
-4248 73  	 	MOV M, E
-4249 C3 5F 42	 	JMP PSSBCK
+SWP:	LXI H, 2001 ;4237 21 D1  7	 
+MOV A, C            ;423A 79  	 	
+ADD L               ;423B 85  	 	
+MOV L, A            ;423C 6F  	 	
+MOV A, M            ;423D 7E  	 	
+MOV M, E            ;423E 73  	 	
+MOV E, A            ;423F 5F  	 	
+LXI H, 2001         ;4240 21 D1  7	 	
+LDA 2012            ;4243 3A DC  7	 	
+ADD L               ;4246 85  	 	
+MOV L, A            ;4247 6F  	 	
+MOV M, E            ;4248 73  	 	
+JMP PSSBCK          ;4249 C3 5F 42	 	
    	 
-424C 7A  	 CHGIDX: MOV A, D
-424D 32 DC  7	 	STA 2012
-4250 C3 30 42	 	JMP BACK
+CHGIDX: MOV A, D    ;424C 7A  	 
+STA 2012            ;424D 32 DC  7	 	
+JMP BACK            ;4250 C3 30 42	 	
    	 
-4253 3A DC  7	 NXTPSS: LDA 2012
-4256 21 D1  7	 	LXI H, 2001
-4259 85  	 	ADD L
-425A 6F  	 	MOV L, A
-425B 5E  	 	MOV E, M
-425C C3 37 42	 	JMP SWP
+NXTPSS: LDA 2012    ;4253 3A DC  7	 
+LXI H, 2001         ;4256 21 D1  7	 	
+ADD L               ;4259 85  	 	
+MOV L, A            ;425A 6F  	 	
+MOV E, M            ;425B 5E  	 	
+JMP SWP             ;425C C3 37 42	 	
    	 
-425F 0C  	 PSSBCK: INR C
-4260 C3 0B 042	 	JMP ILOOP
+PSSBCK: INR C       ;425F 0C  	 
+JMP ILOOP           ;4260 C3 0B 042	 	
    	 
-4263 3E 00 	 END:	MVI A, 0
-4265 32 DC  7	 	STA 2012
-4268 76  	 	HLT
+END: MVI A, 0       ;4263 3E 00 	 
+STA 2012            ;4265 32 DC  7	 	
+HLT                 ;4268 76  	 	
